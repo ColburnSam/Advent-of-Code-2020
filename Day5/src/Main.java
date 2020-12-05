937a78290f77bedc6a8c4f6cbc60fe285a48bd6d
ColburnSam <65149875+ColburnSam@users.noreply.github.com>
2020-12-05 00:01:03 -0800

    Update getID

--- Day5/src/Main.java
+++ Day5/src/Main.java
@@ -6,24 +6,11 @@ import java.util.Set;
 
 public class Main {
 
-    private static final int MAX_ROW_NUMBER = 127;
-    private static final int MAX_COL_NUMBER = 7;
-
     public static int getID(String seat) {
-        int rowNumber = MAX_ROW_NUMBER;
-        int colNumber = MAX_COL_NUMBER;
-
-        for (int i = 0; i < 7; i++) {   // Rows
-            if(seat.charAt(i) == 'F')
-                rowNumber -= Math.pow(2, (6-i));
-        }
-
-        for (int i = 0; i < 3; i++) {   // Cols
-            if(seat.charAt(i + 7) == 'L')
-                colNumber -= Math.pow(2, (2-i));
-        }
-
-        return rowNumber * 8 + colNumber;
+        seat = seat.replaceAll("[BR]", "1").replaceAll("[FL]", "0");
+        int row = Integer.parseInt(seat.substring(0,7), 2);
+        int col = Integer.parseInt(seat.substring(7), 2);
+        return row * 8 + col;
     }
 
     public static int findSeat(int max, Set seatNumbers) {
@@ -35,21 +22,16 @@ public class Main {
     }
 
     public static void main(String[] args) throws FileNotFoundException {
-        File boardingPasses = new File("input.txt");
-        Scanner sc = new Scanner(boardingPasses);
+        Scanner sc = new Scanner(new File("input.txt"));
         Set<Integer> seatNumbers = new HashSet<>();
         int max = 0;
-
         while (sc.hasNext()) {
             int curr = getID(sc.next().trim());
             seatNumbers.add(curr);
             if (max < curr)
                 max = curr;
         }
-
-        System.out.println("Part 1:");
-        System.out.println("The highest seat number on a boarding pass is " + max);
-        System.out.println("\nPart 2:");
-        System.out.println("Your seat number is " + findSeat(max, seatNumbers));
+        System.out.println("Part 1: \nThe highest seat number on a boarding pass is " + max);
+        System.out.println("Part 2: \nYour seat number is " + findSeat(max, seatNumbers));
     }
 }
